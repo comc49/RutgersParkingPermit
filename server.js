9673ff7ad3703d80020b12c987504cfe2f22154b
Brian Koo <bkoo@bricksimple.com>
2018-02-12 14:28:32 -0500

    fixed typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const puppeteer = require('puppeteer');
-const port = process.env.PORT || 8080;
+var port = process.env.PORT || 5000;
 
 var parseUrl = function(url) {
     url = decodeURIComponent(url)
@@ -13,7 +13,6 @@ var parseUrl = function(url) {
 };
 
 app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
 
