9eb8c494efbab7b3af64d526515b10c30f950f96
Brian Koo <bkoo@bricksimple.com>
2018-02-16 23:09:36 -0500

    added dist files

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -103,6 +103,52 @@
             <h5>{{hideFormInput ? 'Show' : 'Hide'}} Sensitive field</h5>
             <v-icon class="peek-icon" @click="hideFormInput = !hideFormInput">remove_red_eye</v-icon>
         </div>
+              <v-speed-dial
+        v-model="fab"
+        :top="top"
+        :bottom="bottom"
+        :right="right"
+        :left="left"
+        :direction="direction"
+        :open-on-hover="hover"
+        :transition="transition"
+      >
+        <v-btn
+          slot="activator"
+          color="blue darken-2"
+          dark
+          fab
+          hover
+          v-model="fab"
+        >
+          <v-icon>account_circle</v-icon>
+          <v-icon>close</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          dark
+          small
+          color="green"
+        >
+          <v-icon>edit</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          dark
+          small
+          color="indigo"
+        >
+          <v-icon>add</v-icon>
+        </v-btn>
+        <v-btn
+          fab
+          dark
+          small
+          color="red"
+        >
+          <v-icon>delete</v-icon>
+        </v-btn>
+      </v-speed-dial>
     </section>
 
 </template>
@@ -158,7 +204,7 @@ export default {
                 this.key = res.data;
                 let bytesCC  = CryptoJS.AES.decrypt(this.userFormData.visaCC.toString(), this.key);
                 let bytesCCCVV2  = CryptoJS.AES.decrypt(this.userFormData.visaCCCVV2.toString(), this.key);
-                let bytesRutgersPassword  = CryptoJS.AES.decrypt(this.userFormData.visaCC.toString(), this.key);
+                let bytesRutgersPassword  = CryptoJS.AES.decrypt(this.userFormData.rutgersPassword.toString(), this.key);
                 this.rutgersPassword = bytesRutgersPassword.toString(CryptoJS.enc.Utf8);
                 this.visaCC = bytesCC.toString(CryptoJS.enc.Utf8);
                 this.visaCCCVV2 = bytesCCCVV2.toString(CryptoJS.enc.Utf8);
